cb1b2240c572182c7076a21e3b312ed1e7bb0644
Stanislav Vorobyev <vissi@vissi.su>
2012-12-17 13:54:56 +0400

    mac-compatible 3mview.sh

--- 3mview/3mview.sh
+++ 3mview/3mview.sh
@@ -1,6 +1,10 @@
-#! /usr/bin/sh
+#!/bin/sh
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${PWD}/ost/stage/lib:
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${PWD}/ost/stage/lib64:
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${PWD}/openbabel/lib:
-./3mview
+if [ -f ./3mview ]; then
+	./3mview
+else
+	open 3mview.app
+fi
