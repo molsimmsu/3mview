1e676e181da82e0e5b6b7bd49d3db3245722aed3
Dimax4d <dmitry.shurov@mail.ru>
2013-02-14 11:42:15 +0400

    Removed VMD ccp4 plugin

--- Voreen/custommodules/electrondensity/electrondensity.cmake
+++ Voreen/custommodules/electrondensity/electrondensity.cmake
@@ -6,7 +6,6 @@ SET(MOD_CORE_MODULECLASS ElectronDensityModule)
 SET(MOD_CORE_SOURCES
     ${MOD_DIR}/processors/segmentationbase.cpp
     ${MOD_DIR}/io/mrcvolumereader.cpp
-    ${MOD_DIR}/io//vmd/ccp4plugin.cpp
 )
 
 # module's core header files, path relative to module dir
