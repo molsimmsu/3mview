87a71b57dc862cb586f5e321e4c4c13718fee782
benji88 <blinov.veniamin@gmail.com>
2013-02-19 01:11:40 -0800

    Create alignbymoments.h
    заголовок процессора, который получает облако точек и считает центр и оси инерции (в каком формате это дело выводить пока непонятно - можно прямо в том же виде облака 4х точек)

---
+++ Voreen/custommodules/formcharacteristics/processors/alignbymoments.h
@@ -0,0 +1,49 @@
+#ifndef VRN_MOLECULESOURCE_H
+#define VRN_MOLECULESOURCE_H
+
+#include "../ports/weightedpointcloudport.h"
+
+#include "voreen/core/processors/processor.h"
+using namespace voreen;
+
+class AlignByMoments : public Processor {
+public:
+    AlignByMoments();
+
+    // virtual constructor
+    virtual Processor* create() const { return new AlignByMoments(); }
+
+    // documentary functions
+    virtual std::string getClassName() const { return "AlignByMoments";        }
+    virtual std::string getCategory()  const { return "Form Analysis";         }
+    virtual CodeState getCodeState()   const { return CODE_STATE_EXPERIMENTAL; }
+
+    double  O[3]; 
+    double Ox[3];
+    double Oy[3];
+    double Oz[3];
+    double polunom[3];
+    double total_weight;
+    int    entries;
+
+protected:
+    virtual void setDescriptions() {
+        setDescription("Accepts WeigthedPointCloud as input, outputs a matrix as a WeigthedPointCloud");
+    }
+
+    virtual void process() {}
+
+private:
+
+    WeightedPointCloudPort inport_;
+    WeightedPointCloudPort outport_;   
+
+    double CalculateMoment(int, int, int);
+    double CalculateFourrier(int, int, int);
+    void   FillOutport();  			// TODO
+    void   FindAxes();
+    double PolynomVal(double);
+   
+};
+
+#endif // VRN_MOLECULESOURCE_H
