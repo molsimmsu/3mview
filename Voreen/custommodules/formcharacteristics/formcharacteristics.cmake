d717fbf6b5461c8969bab507788eb2d475e8455a
benji88 <blinov.veniamin@gmail.com>
2013-03-11 01:41:36 +0400

    Update formcharacteristics.cmake

--- Voreen/custommodules/formcharacteristics/formcharacteristics.cmake
+++ Voreen/custommodules/formcharacteristics/formcharacteristics.cmake
@@ -3,20 +3,14 @@ SET(MOD_CORE_MODULECLASS FormCharacteristicsModule)
 
 # module's core source files, path relative to module dir
 SET(MOD_CORE_SOURCES
-    ${MOD_DIR}/datastructures/weightedpointcloud.cpp
-    ${MOD_DIR}/ports/weightedpointcloudport.cpp
     ${MOD_DIR}/ports/matrix4doubleport.cpp
-    #${MOD_DIR}/processors/pointcloudcharacteristics.cpp
     ${MOD_DIR}/processors/alignbymoments.cpp
     ${MOD_DIR}/processors/databasematch.cpp
 )
 
 # module's core header files, path relative to module dir
 SET(MOD_CORE_HEADERS
-    ${MOD_DIR}/datastructures/weightedpointcloud.h
-    ${MOD_DIR}/ports/weightedpointcloudport.h
     ${MOD_DIR}/ports/matrix4doubleport.h
-    #${MOD_DIR}/processors/pointcloudcharacteristics.h
     ${MOD_DIR}/processors/alignbymoments.h
     ${MOD_DIR}/processors/databasematch.h
 )   
