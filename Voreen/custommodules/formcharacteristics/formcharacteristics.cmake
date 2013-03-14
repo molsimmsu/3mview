7d3e39b75a5af5a0ff0fe8f81196bb3771ddee9b
benji88 <blinov.veniamin@gmail.com>
2013-03-15 03:39:59 +0400

    Update formcharacteristics.cmake

--- Voreen/custommodules/formcharacteristics/formcharacteristics.cmake
+++ Voreen/custommodules/formcharacteristics/formcharacteristics.cmake
@@ -6,6 +6,7 @@ SET(MOD_CORE_SOURCES
     ${MOD_DIR}/ports/matrix4doubleport.cpp
     ${MOD_DIR}/processors/alignbymoments.cpp
     ${MOD_DIR}/processors/databasematch.cpp
+    ${MOD_DIR}/processors/calculatescore.cpp
 )
 
 # module's core header files, path relative to module dir
@@ -13,4 +14,5 @@ SET(MOD_CORE_HEADERS
     ${MOD_DIR}/ports/matrix4doubleport.h
     ${MOD_DIR}/processors/alignbymoments.h
     ${MOD_DIR}/processors/databasematch.h
+    ${MOD_DIR}/processors/calculatescore.h
 )   
