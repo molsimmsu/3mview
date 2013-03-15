dc567ec9fc6ee917c2d1051fe19bd59c95805c53
Dimax4d <dmitry.shurov@mail.ru>
2013-03-15 15:35:55 +0400

    Removed old files from electron density module

--- Voreen/custommodules/electrondensity/electrondensity.cmake
+++ Voreen/custommodules/electrondensity/electrondensity.cmake
@@ -2,7 +2,6 @@ SET(MOD_CORE_MODULECLASS ElectronDensityModule)
 
 # module's core source files, path relative to module dir
 SET(MOD_CORE_SOURCES
-    ${MOD_DIR}/processors/segmentationbase.cpp
     ${MOD_DIR}/processors/segmentationprocessor.cpp
     ${MOD_DIR}/processors/densitymapcoprocessor.cpp
     ${MOD_DIR}/processors/densitymapmanipulation.cpp
@@ -13,7 +12,6 @@ SET(MOD_CORE_SOURCES
 
 # module's core header files, path relative to module dir
 SET(MOD_CORE_HEADERS
-    ${MOD_DIR}/processors/segmentationbase.h
     ${MOD_DIR}/processors/segmentationprocessor.h
     ${MOD_DIR}/processors/densitymapcoprocessor.h
     ${MOD_DIR}/processors/densitymapmanipulation.h
