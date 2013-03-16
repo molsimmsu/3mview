fbfc48fd44f595f57273ff696b1a62aee457d5a1
Dimax4d <dmitry.shurov@mail.ru>
2013-03-16 17:55:31 +0400

    Removed obsolete files from FormCharacteristics module

--- Voreen/custommodules/formcharacteristics/ports/weightedpointcloudport.cpp
+++
@@ -1,7 +0,0 @@
-#include "weightedpointcloudport.h"
-
-WeightedPointCloudPort::WeightedPointCloudPort(PortDirection direction, const std::string& name, const std::string& guiName,
-             bool allowMultipleConnections,
-             Processor::InvalidationLevel invalidationLevel)
-: GenericPort<WeightedPointCloud>(direction, name, guiName, allowMultipleConnections, invalidationLevel)
-{}
