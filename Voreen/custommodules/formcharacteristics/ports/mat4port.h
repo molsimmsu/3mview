72e14245e05290380a09299178e96756f46df633
benji88 <blinov.veniamin@gmail.com>
2013-03-21 11:50:38 +0400

    Update mat4port.h

--- Voreen/custommodules/formcharacteristics/ports/mat4port.h
+++ Voreen/custommodules/formcharacteristics/ports/mat4port.h
@@ -1,26 +1,94 @@
-#ifndef VRN_MATRIXPORT_H
-#define VRN_MATRIXPORT_H
+//    Moments calculation application, September, 17 2012	
+//                     
 
-#include "tgt/matrix.h"
-#include "voreen/core/ports/genericport.h"
+#include <stdio.h>
+#include <cmath>
+#include <cstdlib>
+#include <string.h>
 
-using namespace voreen;
+/*  MAX_ORDER defines maximum degree of moments to be calculated                           */
+/*  WEIGHT_RENORM defines wether weight renormalization of moments is to be performed      */  
 
-#ifdef DLL_TEMPLATE_INST
-template class VRN_CORE_API GenericPort<tgt::mat4>;
-#endif
+#define    MAX_ORDER  		8
+#define    WEIGHT_RENORM		0
 
-class VRN_CORE_API mat4Port : public GenericPort<tgt::mat4> {
-public:
-    mat4Port(PortDirection direction, const std::string& name, const std::string& guiName = "",
-                 bool allowMultipleConnections = false,
-                 Processor::InvalidationLevel invalidationLevel = Processor::INVALID_PROGRAM);
-                 
-    virtual std::string getClassName() const { return "mat4Port"; }
-    
-    virtual bool supportsCaching() const { return false; }
+/*  atoms weights register  */
+#define    C_WEIGHT           12
+#define    N_WEIGHT 		14
+#define    O_WEIGHT 		16
+#define    H_WEIGHT 		1
+#define    F_WEIGHT 		0
+#define    P_WEIGHT 		0
+#define    S_WEIGHT 		32
+#define    MG_WEIGHT 		0
+#define    MN_WEIGHT 		0
+#define    Z_WEIGHT			0
+#define    UNCOMMON_WEIGHT 	0
 
-    virtual tgt::col3 getColorHint() const { return tgt::col3(255, 200, 255); }
+#define    CA_ONLY            1  
+
+#define    SCALE              30
+#define    MOM_TYPE			1     // type of moments 0 - geometrical; 1 - fourrier; 2 - spherical
+//#define 	 BOX_R			1.5
+#define	 PI_2		 	1.570796327
+#define    MAX_SIZE			1.5     // for fourrier 
+
+struct particle {
+	int    weight;
+
+
+
+	double x, y, z;
+
+
+	char   number[7];
+	char   type[5];
+	char   altloc[2];
+	char   residue[5];
+	char   chain[2];
+	char   resseq[5];
+	char   icode[2];
 };
 
-#endif // VRN_MATRIXPORT_H
+class PDB {
+public:
+	particle *atoms;
+	int 		mom_total;
+
+	int 	  entries;
+	char   name[10];
+	int 	  total_weight;
+	double    scale;
+		
+	double    Ox[3];
+	double    Oy[3];
+	double    Oz[3];
+	double     O[3];
+	
+	
+	double    I[3][3];
+	double    polynom[4];
+	double    eigens[3];
+	
+	double    moments[1000];
+
+	
+	PDB();
+	PDB(char * arg);
+	~PDB();
+
+	void Read(char * arg);
+	void WritePDB();
+	
+	double CalculateMoment(int degX, int degY, int degZ);
+	double CalculateFourrier(int degX, int degY, int degZ);
+	double CalculateSpherical(int degX, int degY, int degZ);
+	double PolynomVal(double x);
+
+	void   Center();
+	void   Rescale();
+	void   Reorientate();
+	void   Reduce();
+	void   GetMoments();
+	void   WriteMoments();
+};
