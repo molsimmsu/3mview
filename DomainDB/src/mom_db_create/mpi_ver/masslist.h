211d35253dda0f8d64e9aa182ef2b89e868f9c2d
benji88 <blinov.veniamin@gmail.com>
2013-03-26 13:32:42 +0400

    Create masslist.h

---
+++ DomainDB/src/mom_db_create/mpi_ver/masslist.h
@@ -0,0 +1,16 @@
+/*  atoms weights register  */
+#define    C_WEIGHT           6
+#define    CL_WEIGHT          17
+#define    N_WEIGHT   	7
+#define    NA_WEIGHT 		11
+#define    O_WEIGHT 		8
+#define    H_WEIGHT 		1
+#define    F_WEIGHT 		9
+#define    FE_WEIGHT 		26
+#define    P_WEIGHT 		15
+#define    S_WEIGHT 		16
+#define    MG_WEIGHT 		12
+#define    MN_WEIGHT 		25
+#define    Z_WEIGHT			30
+#define    HET_WEIGHT		0
+#define    UNCOMMON_WEIGHT 	1
