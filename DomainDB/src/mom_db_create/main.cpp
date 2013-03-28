554feea0d086a484868892c2a99121cb706d86a7
benji88 <blinov.veniamin@gmail.com>
2013-03-28 16:45:46 +0400

    Update main.cpp

--- DomainDB/src/mom_db_create/main.cpp
+++ DomainDB/src/mom_db_create/main.cpp
@@ -1,5 +1,4 @@
 #include "pdb.h"
-#include <ctime>
 
 int main(int args, char ** argv)
 {
@@ -8,10 +7,8 @@ int main(int args, char ** argv)
 		printf("No argument specified!\n");
 		exit(-1);
 	}
-	clock_t now = clock();
 	PDB temp(argv[1]);
 	temp.Reduce();
 	temp.WriteMoments();
-	printf("\ntime = %.2lf seconds\n", ((double)(clock()-now))/CLOCKS_PER_SEC);
 	return 0;
 }
