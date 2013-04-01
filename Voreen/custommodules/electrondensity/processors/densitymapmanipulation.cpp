a9d5f14f757c1dbcc41bb79dce4bb795bb983609
Satary <satary@satary-Home-Ubuntu.(none)>
2013-04-02 02:00:52 +0400

    volumes rotates along their center

--- Voreen/custommodules/electrondensity/processors/densitymapmanipulation.cpp
+++ Voreen/custommodules/electrondensity/processors/densitymapmanipulation.cpp
@@ -25,8 +25,25 @@ void DensityMapManipulation::applyTransformation(tgt::vec3 offset, tgt::mat4 tra
             offsetMatrix[1][3] = offset[1];
             offsetMatrix[2][3] = offset[2];
             
+            tgt::ivec3 center = dynamic_cast<Volume*>(volume)->getDimensions();
+            center = dynamic_cast<Volume*>(volume)->getVoxelToWorldMatrix() * tgt::vec3(center[0] / 2.0, center[1] / 2.0, center[2] / 2.0);
+
+            tgt::mat4 shiftMatrix(
+                1, 0, 0, -center[0],
+                0, 1, 0, -center[1],
+                0, 0, 1, -center[2],
+                0, 0, 0, 1
+            );
+            
+            tgt::mat4 returnMatrix(
+                1, 0, 0, center[0],
+                0, 1, 0, center[1],
+                0, 0, 1, center[2],
+                0, 0, 0, 1
+            );
+            
             tgt::mat4 matrix = volume->getPhysicalToWorldMatrix();
-            dynamic_cast<Volume*>(volume)->setPhysicalToWorldMatrix(offsetMatrix * transform * matrix);
+            dynamic_cast<Volume*>(volume)->setPhysicalToWorldMatrix(returnMatrix * offsetMatrix * transform * shiftMatrix* matrix);
         }
         
         DensityMapCollectionSource* source = getSourceProcessor();
